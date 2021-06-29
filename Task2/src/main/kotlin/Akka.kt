663a305b307fc0e9c8ae349d49f3c6086c6a8bf7
nikita <nikita03565@gmail.com>
2021-06-29 13:50:10 +0300

    update results

--- Task2/src/main/kotlin/Akka.kt
+++ Task2/src/main/kotlin/Akka.kt
@@ -11,7 +11,7 @@ data class CountFactors(val countFactors: Int)
 class MasterActor : AbstractActor() {
     var workersCount = 0
     var finished = 0
-    var count = 0L
+    var res = 0
     var workers = Array<ActorRef>(workersCount) {
         context.actorOf(Props.create(SlaveActor::class.java))
     }
@@ -28,10 +28,10 @@ class MasterActor : AbstractActor() {
                 }
             }
             .match(CountFactors::class.java) {
-                count += it.countFactors
+                res += it.countFactors
                 finished++
                 if (finished == workersCount) {
-                    println("Akka result: $count")
+                    println("Akka result: $res")
                     context.system.terminate()
                 }
             }
@@ -48,10 +48,58 @@ class SlaveActor : AbstractActor() {
             .build()
 }
 
+class MasterActorAlt : AbstractActor() {
+    var workersCount = 0
+    var finished = 0
+    var res = 0
+    var workers = Array<ActorRef>(workersCount) {
+        context.actorOf(Props.create(SlaveActorAlt::class.java))
+    }
+
+    override fun createReceive(): Receive =
+        ReceiveBuilder()
+            .match(File::class.java) { file ->
+                val lines = file.readLines()
+                workersCount = lines.size
+                workers = Array(workersCount) { context.actorOf(Props.create(SlaveActorAlt::class.java)) }
+
+                for (i in 0 until workersCount) {
+                    workers[i].tell(lines[i], self)
+                }
+            }
+            .match(CountFactors::class.java) {
+                res += it.countFactors
+                finished++
+                if (finished == workersCount) {
+                    println("Akka result alt: $res")
+                    context.system.terminate()
+                }
+            }
+            .build()
+}
+
+class SlaveActorAlt : AbstractActor() {
+    override fun createReceive(): Receive =
+        ReceiveBuilder()
+            .match(String::class.java) {
+                val countFactors: Int = getPrimeFactorsCountAlt(BigInteger(it))
+                sender.tell(CountFactors(countFactors), self)
+            }
+            .build()
+}
+
 fun countAkka() {
     val file = File(fileName)
     val actorSystem = ActorSystem.create("AkkaFactorization")
     val actorRef = actorSystem.actorOf(Props.create(MasterActor::class.java))
+    actorRef.tell(file, actorRef)
+    while (!actorRef.isTerminated) Thread.sleep(1)
+}
+
+fun countAkkaAlt() {
+    val file = File(fileName)
+    val actorSystem = ActorSystem.create("AkkaFactorizationAlt")
+    val actorRef = actorSystem.actorOf(Props.create(MasterActorAlt::class.java))
     actorRef.tell(file, actorRef)
     while (!actorRef.isTerminated) Thread.sleep(1)
 }
--- Task2/src/main/kotlin/main.kt
+++ Task2/src/main/kotlin/main.kt
@@ -18,32 +18,40 @@ fun main() {
     countTime ({ RxFlowable(::getPrimeFactorsCountAlt)})
     countTime ({ countCompletableFuture(::getPrimeFactorsCountAlt) })
     countTime ({ countCompletableFutureGlobal(::getPrimeFactorsCountAlt) })
+    countTime ({ countAkkaAlt() })
 }
 
 
 ///////////////////////////////// Output /////////////////////////////////
 /*
 Simple result: 9029 with function: function getPrimeFactorsCount
-Elapsed time 6865 ms
+Elapsed time 6787 ms
 
 RxFlowable: 9029 with function: function getPrimeFactorsCount
-Elapsed time 3230 ms
+Elapsed time 3220 ms
 
 CompletableFuture result: 9029 with function function getPrimeFactorsCount
-Elapsed time 3199 ms
+Elapsed time 3186 ms
 
 CompletableFuture with Global var result: 9029 with function function getPrimeFactorsCount
-Elapsed time 3146 ms
+Elapsed time 3171 ms
+
+Akka result: 9029
+Elapsed time 3936 ms
 
 Simple result: 9029 with function: function getPrimeFactorsCountAlt
-Elapsed time 55732 ms
+Elapsed time 53547 ms
 
 RxFlowable: 9029 with function: function getPrimeFactorsCountAlt
-Elapsed time 25733 ms
+Elapsed time 26241 ms
 
 CompletableFuture result: 9029 with function function getPrimeFactorsCountAlt
-Elapsed time 26271 ms
+Elapsed time 26344 ms
 
 CompletableFuture with Global var result: 9029 with function function getPrimeFactorsCountAlt
-Elapsed time 26296 ms
+Elapsed time 26463 ms
+
+Akka result alt: 9029
+Elapsed time 26493 ms
+
  */
